0563891fed9163209f30173594a806ef94387de6
yoonki1207 <yoonki1207@gmail.com>
2026-06-10 17:48:08 +0900

    feat: FE를 위한 CORS 처리 추가

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from database import Base, engine
 from routes.auth import router as auth_router
 from routes.users import router as users_router
 import model
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 Base.metadata.create_all(bind=engine)
 app.include_router(auth_router)
 app.include_router(users_router)
