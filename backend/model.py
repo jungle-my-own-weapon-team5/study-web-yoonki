8e0bb91590b876d4113c67d86f0f1c154edf8759
yoonki1207 <yoonki1207@gmail.com>
2026-06-11 17:09:05 +0900

    refactor: refine SQLAlchemy model relationships, category 낙관적락, 변수스타일변경 - TagRelation, BoardLike - category 낙관적 락 적용 - updatedAt, createdAt -> updated_at, create_at : python스타일 적용

--- backend/model.py
+++ backend/model.py
@@ -4,8 +4,8 @@ from database import Base
 
 class BaseEntity:
     id = Column(Integer, primary_key=True, autoincrement=True)
-    createdAt = Column(DateTime, nullable=False, server_default=func.now())
-    updatedAt = Column(DateTime, nullable=True, onupdate=func.now())
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
+    updated_at = Column(DateTime, nullable=True, onupdate=func.now())
 
 class User(BaseEntity, Base):
     __tablename__ = 'users'
@@ -19,6 +19,7 @@ class User(BaseEntity, Base):
 
     boards = relationship("Board", back_populates="user")
     comments = relationship("Comment", back_populates="user")
+    board_likes = relationship("BoardLike", back_populates="user")
     
 class Board(BaseEntity, Base):
     __tablename__ = 'board'
@@ -36,13 +37,19 @@ class Board(BaseEntity, Base):
     category_id = Column(Integer, ForeignKey("category.id"), nullable=False)
     category = relationship("Category", back_populates="boards")
 
+    tag_relation = relationship("TagRelation", back_populates="board")
+
+    likes = relationship("BoardLike", back_populates="board")
+
     comments = relationship("Comment", back_populates="board")
 
 class BoardLike(Base):
     __tablename__ = 'board_like'
 
     user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    user = relationship("User", back_populates="board_likes")
     board_id = Column(Integer, ForeignKey("board.id"), primary_key=True)
+    board = relationship("Board", back_populates="likes")
 
 class Tag(BaseEntity, Base):
     __tablename__ = 'tag'
@@ -50,6 +57,7 @@ class Tag(BaseEntity, Base):
     title = Column(String(250), nullable=False)
 
     version = Column(Integer, nullable=False, default=1)
+    tag_relation = relationship("TagRelation", back_populates="tag")
 
     __mapper_args__ = {"version_id_col": version}
 
@@ -57,12 +65,17 @@ class TagRelation(Base):
     __tablename__ = 'tag_relation'
 
     board_id = Column(Integer, ForeignKey("board.id"), primary_key=True)
+    board = relationship("Board", back_populates="tag_relation")
     tag_id = Column(Integer, ForeignKey("tag.id"), primary_key=True)
+    tag = relationship("Tag", back_populates="tag_relation")
 
 class Category(BaseEntity, Base):
     __tablename__ = 'category'
 
     title = Column(String(250), nullable=False)
+    version = Column(Integer, nullable=False, default=1)
+
+    __mapper_args__ = {"version_id_col": version}
 
     boards = relationship("Board", back_populates="category")
 
